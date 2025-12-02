c0efb1090d586d41c07d49890a5e9a99038b4051
RLASH18 <lacdangryan18@gmail.com>
2025-12-02 14:02:32 +0800

    add base repository and interface

---
+++ app/Repositories/Interfaces/BaseRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseRepositoryInterface
+{
+    public function all(): Collection;
+    public function find(int $id): ?Model;
+    public function paginate(int $perPage = 10): LengthAwarePaginator;
+    public function create(array $data): Model;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+}
